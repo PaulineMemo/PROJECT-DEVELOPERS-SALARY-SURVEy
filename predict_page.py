c9b6df25f63c41d848480690e75aee026d9bcfba
PaulineMemo <103488901+PaulineMemo@users.noreply.github.com>
2022-11-01 18:05:59 +0300

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -10,12 +10,12 @@ import numpy as np
 
 
 
-#def load_model():
-   # with open('saved_steps.pkl', 'rb') as file:
-        #data = pickle.load(file)
-    #return data
+def load_model():
+    with open('saved_steps.pkl', 'rb') as file:
+        data = pickle.load(file)
+    return data
 
-#data = load_model()
+data = load_model()
 
 regressor = data["model"]
 le_country = data["le_country"]
