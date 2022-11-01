cb98b926ae4f7231c798b7981d8d68a1f72a4552
PaulineMemo <103488901+PaulineMemo@users.noreply.github.com>
2022-11-01 18:03:23 +0300

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -10,10 +10,10 @@ import numpy as np
 
 
 
-def load_model():
-    with open('saved_steps.pkl', 'rb') as file:
-        data = pickle.load(file)
-    return data
+#def load_model():
+   # with open('saved_steps.pkl', 'rb') as file:
+        #data = pickle.load(file)
+    #return data
 
 data = load_model()
 
